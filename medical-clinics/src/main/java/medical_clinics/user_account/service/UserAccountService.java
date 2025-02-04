2ff443043c4acb7c32ce80728994fbe236169de5
359883354079 <145838890+Dimitar-Tsanev@users.noreply.github.com>
2025-02-04 18:22:33 +0200

    Refactor register to search for existing physician add methods to change account email to find if is email is present and get by email

--- medical-clinics/src/main/java/medical_clinics/user_account/service/UserAccountService.java
+++ medical-clinics/src/main/java/medical_clinics/user_account/service/UserAccountService.java
@@ -1,7 +1,10 @@
 package medical_clinics.user_account.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import medical_clinics.patient.service.PatientService;
+import medical_clinics.physician.service.PhysicianService;
+import medical_clinics.shared.exception.UserAccountNotFoundException;
 import medical_clinics.shared.exception.UserAlreadyExistsException;
 import medical_clinics.user_account.mapper.UserAccountMapper;
 import medical_clinics.user_account.model.UserAccount;
@@ -21,18 +24,42 @@ public class UserAccountService {
     private final PatientService patientService;
     private final PasswordEncoder passwordEncoder;
     private final UserProperty userProperty;
+    private final PhysicianService physicianService;
 
+    @Transactional
     public void register ( RegisterRequest registerRequest ) {
         Optional<UserAccount> user = userAccountRepository.findByEmail ( registerRequest.getEmail ( ) );
 
         if ( user.isPresent ( ) ) {
             throw new UserAlreadyExistsException ( "User with this email already exists" );
         }
-
         UserAccount userAccount = userAccountRepository.save (
                 UserAccountMapper.registrationMapper ( registerRequest, userProperty, passwordEncoder )
         );
+        boolean isPhysicianEmail = physicianService.isEmailOfPhysician (registerRequest.getEmail ());
 
+        if (isPhysicianEmail) {
+            physicianService.addPhysicianAccount(registerRequest, userAccount );
+            return;
+        }
         patientService.addPatientAccount ( registerRequest, userAccount );
+    }
+
+    public Optional<UserAccount> getAccountIdByEmail ( String email ) {
+        return userAccountRepository.findByEmail ( email );
+    }
+
+    public void changeAccountEmail ( String oldEmail, String newEmail ) {
+        UserAccount userAccount = userAccountRepository.findByEmail ( oldEmail )
+                .orElseThrow (()->
+                       new UserAccountNotFoundException ( "User with provided email does not exist" )
+                );
+
+        userAccount.setEmail ( newEmail );
+        userAccountRepository.save(userAccount);
+    }
+
+    public boolean findByEmail ( String email ) {
+        return userAccountRepository.findByEmail ( email ).isPresent ( );
     }
 }
