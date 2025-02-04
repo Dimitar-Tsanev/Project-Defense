9c1b35871395058dcf83c5acb0f0d01e34e88071
359883354079 <145838890+Dimitar-Tsanev@users.noreply.github.com>
2025-02-04 18:02:43 +0200

    add map from string name to enum

--- medical-clinics/src/main/java/medical_clinics/specialty/mapper/SpecialityMapper.java
+++ medical-clinics/src/main/java/medical_clinics/specialty/mapper/SpecialityMapper.java
@@ -1,15 +1,23 @@
 package medical_clinics.specialty.mapper;
 
 import medical_clinics.specialty.model.Specialty;
+import medical_clinics.specialty.model.SpecialtyName;
 import medical_clinics.web.dto.SpecialityDto;
 
 public class SpecialityMapper {
-    private SpecialityMapper() {}
+    private SpecialityMapper () {
+    }
 
-    public static SpecialityDto mapToDto( Specialty specialty ) {
+    public static SpecialityDto mapToDto ( Specialty specialty ) {
         return SpecialityDto.builder ( )
-                .id ( specialty.getId () )
-                .name ( specialty.getName ( ).toString () )
+                .id ( specialty.getId ( ) )
+                .name ( specialty.getName ( ).toString ( ) )
                 .build ( );
+    }
+
+    public static SpecialtyName mapNameFromString ( String specialtyName ) {
+        return SpecialtyName.valueOf (
+                specialtyName.toUpperCase ( ).replaceAll ( "[\\s-.]", "_" )
+        );
     }
 }
