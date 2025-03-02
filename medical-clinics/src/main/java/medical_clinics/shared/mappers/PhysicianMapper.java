8edb962093a89e87e32a09285318effcd955ae34
359883354079 <145838890+Dimitar-Tsanev@users.noreply.github.com>
2025-03-02 18:47:18 +0200

    Add map to Export physician DTO

--- medical-clinics/src/main/java/medical_clinics/shared/mappers/PhysicianMapper.java
+++ medical-clinics/src/main/java/medical_clinics/shared/mappers/PhysicianMapper.java
@@ -2,6 +2,7 @@ package medical_clinics.shared.mappers;
 
 import medical_clinics.physician.model.Physician;
 import medical_clinics.web.dto.CreatePhysician;
+import medical_clinics.web.dto.PhysicianShortInfo;
 
 public class PhysicianMapper {
     private PhysicianMapper() {}
@@ -18,4 +19,24 @@ public class PhysicianMapper {
                 .build ( );
     }
 
+    public static PhysicianShortInfo mapToPhysicianShortInfo( Physician physician ) {
+        String workplaceAddress = physician.getWorkplace ().getCity () +
+                ", " +
+                physician.getWorkplace ( ).getAddress ();
+
+        String specialtyName = physician.getSpecialty ().getName ().name ();
+        String specialtyNameFormatted = specialtyName.charAt (0 ) +
+                specialtyName.substring (1).toLowerCase (  );
+
+        return PhysicianShortInfo.builder ( )
+                .id ( physician.getId ( ) )
+                .firstName ( physician.getFirstName ( ) )
+                .lastName ( physician.getLastName ( ) )
+                .abbreviation ( physician.getAbbreviation ( ) )
+                .pictureUrl ( physician.getPictureUrl ( ) )
+                .description ( physician.getDescription ( ) )
+                .workplace ( workplaceAddress )
+                .specialty ( specialtyNameFormatted )
+                .build ( );
+    }
 }
