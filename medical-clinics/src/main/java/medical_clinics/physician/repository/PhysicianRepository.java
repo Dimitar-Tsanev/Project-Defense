76ef8f5f919b67a842191f1bbf71fc1ade84c44c
359883354079 <145838890+Dimitar-Tsanev@users.noreply.github.com>
2025-03-02 18:49:30 +0200

    Generate ability to get List of physicians by there speciality and workplace

--- medical-clinics/src/main/java/medical_clinics/physician/repository/PhysicianRepository.java
+++ medical-clinics/src/main/java/medical_clinics/physician/repository/PhysicianRepository.java
@@ -4,6 +4,7 @@ import medical_clinics.physician.model.Physician;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -12,6 +13,8 @@ public interface PhysicianRepository extends JpaRepository<Physician, UUID> {
     Optional<Physician> findByIdentificationNumber ( String identificationNumber );
 
     Optional<Physician> findByEmail ( String email );
+
+    List<Physician> findAllByWorkplace_IdAndSpecialty_Id (UUID clinicId, UUID specialityId);
 
     Optional<Physician> findByUserAccount_Id ( UUID userAccountId );
 }
