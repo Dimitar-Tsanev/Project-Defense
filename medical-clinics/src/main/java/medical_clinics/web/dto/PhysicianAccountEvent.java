722303206d2820f1721d50247f5897069bb883ce
359883354079 <145838890+Dimitar-Tsanev@users.noreply.github.com>
2025-03-02 18:46:25 +0200

    Rename Event for proper use

--- medical-clinics/src/main/java/medical_clinics/web/dto/PhysicianAccountEvent.java
+++ medical-clinics/src/main/java/medical_clinics/web/dto/PhysicianAccountEvent.java
@@ -2,10 +2,10 @@ package medical_clinics.web.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import medical_clinics.physician.model.Physician;
 
 @AllArgsConstructor
 @Getter
-public class NewPhysicianEvent {
-    private Physician physician;
+
+public class PhysicianAccountEvent {
+    private String email;
 }
