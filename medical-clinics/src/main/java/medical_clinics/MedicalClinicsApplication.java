951360070ec0dca809ff946a50baddf820248c9f
359883354079 <145838890+Dimitar-Tsanev@users.noreply.github.com>
2025-03-09 18:07:58 +0200

    Enable feign client

--- medical-clinics/src/main/java/medical_clinics/MedicalClinicsApplication.java
+++ medical-clinics/src/main/java/medical_clinics/MedicalClinicsApplication.java
@@ -3,10 +3,12 @@ package medical_clinics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableFeignClients
 @ConfigurationPropertiesScan
 public class MedicalClinicsApplication {
 
