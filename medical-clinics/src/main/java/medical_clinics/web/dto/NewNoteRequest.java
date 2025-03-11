fe10f37011d464db45aa5dcf4fc720c1718645fb
359883354079 <145838890+Dimitar-Tsanev@users.noreply.github.com>
2025-03-11 21:55:33 +0200

    add dto for Note creation

---
+++ medical-clinics/src/main/java/medical_clinics/web/dto/NewNoteRequest.java
@@ -0,0 +1,37 @@
+package medical_clinics.web.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+
+public class NewNoteRequest {
+
+    @NotBlank(message = "{not.blank}")
+    @Size(min = 5, max = 255, message = "{diagnosis.length}")
+    @Pattern(regexp = "[A-Za-z0-9,.\\- _]", message = "{diagnosis.unsupported.characters}")
+    private String diagnosis;
+
+    @Pattern(regexp = "^[A-Z]\\d{2}\\.\\d$", message = "{diagnosis.code}")
+    private String diagnosisCode;
+
+    private String chiefComplaint;
+
+    @NotBlank(message = "{not.blank}")
+    private String medicalHistory;
+
+    @NotBlank(message = "{not.blank}")
+    private String examination;
+
+    private String medicationAndRecommendations;
+
+    private String testResults;
+}
