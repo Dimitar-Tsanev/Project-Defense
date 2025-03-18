adab148dbd300d5ffa8b6b412dd1a7c8632f06dd
359883354079 <145838890+Dimitar-Tsanev@users.noreply.github.com>
2025-03-18 21:12:13 +0200

    Add documentation, replace lombok @Data with @Getter @Setter, rename some variable

--- medical-clinics/src/main/java/medical_clinics/schedule/models/TimeSlot.java
+++ medical-clinics/src/main/java/medical_clinics/schedule/models/TimeSlot.java
@@ -1,10 +1,7 @@
 package medical_clinics.schedule.models;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import medical_clinics.patient.model.Patient;
 
 import java.time.LocalTime;
@@ -13,7 +10,8 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Data
+@Getter
+@Setter
 
 @Entity
 public class TimeSlot {
