22594bdaad28f2d8b46290f39c1145df86d1a5c7
359883354079 <145838890+Dimitar-Tsanev@users.noreply.github.com>
2025-04-01 19:41:29 +0300

    remove unused imports

--- medical-clinics-frontend/src/app/pages/main/main.component.ts
+++ medical-clinics-frontend/src/app/pages/main/main.component.ts
@@ -1,7 +1,5 @@
 import {Component} from '@angular/core';
 import {ClinicListComponent} from '../clinic-list/clinic-list.component';
-import {NavComponent} from '../../common/nav/nav.component';
-import {LoginComponent} from '../login/login.component';
 import {AuthenticationComponent} from "../../common/authentication/authentication.component";
 import {EditProfileComponent} from "../edit-profile/edit-profile.component";
 import {ProfileComponent} from "../profile/profile.component";
