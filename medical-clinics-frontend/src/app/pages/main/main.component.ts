917e2f3e01bc576866474a8707dc7c88558b905b
359883354079 <145838890+Dimitar-Tsanev@users.noreply.github.com>
2025-04-01 21:19:29 +0300

    fix the bug app don't allow you to continue without login user

--- medical-clinics-frontend/src/app/pages/main/main.component.ts
+++ medical-clinics-frontend/src/app/pages/main/main.component.ts
@@ -1,16 +1,10 @@
 import {Component} from '@angular/core';
 import {ClinicListComponent} from '../clinic-list/clinic-list.component';
-import {AuthenticationComponent} from "../../common/authentication/authentication.component";
-import {EditProfileComponent} from "../edit-profile/edit-profile.component";
-import {ProfileComponent} from "../profile/profile.component";
 
 @Component({
   selector: 'app-main',
     imports: [
-        ClinicListComponent,
-        AuthenticationComponent,
-        EditProfileComponent,
-        ProfileComponent
+        ClinicListComponent
     ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
