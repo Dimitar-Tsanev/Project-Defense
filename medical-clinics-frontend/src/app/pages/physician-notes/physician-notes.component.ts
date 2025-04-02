bfa738cc6012d5508dca97cf3793dabdc64118ec
359883354079 <145838890+Dimitar-Tsanev@users.noreply.github.com>
2025-04-02 17:18:38 +0300

    Add pages for access medical records

---
+++ medical-clinics-frontend/src/app/pages/physician-notes/physician-notes.component.ts
@@ -0,0 +1,46 @@
+import {Component, OnInit} from '@angular/core';
+import {LoaderComponent} from "../../common/loader/loader.component";
+import {Router, RouterLink} from "@angular/router";
+import {NoteResponse} from '../../services/models/note-response';
+import {AuthService} from '../../services/my-services/auth-service';
+import {MedicalRecordControllerService} from '../../services/services/medical-record-controller.service';
+
+@Component({
+  selector: 'app-physician-notes',
+    imports: [
+        LoaderComponent
+    ],
+  templateUrl: './physician-notes.component.html',
+  styleUrl: './physician-notes.component.css'
+})
+export class PhysicianNotesComponent implements OnInit {
+  accountId: string = '';
+  outpatientSheets: Array<NoteResponse> = [];
+  isLoading: boolean = true;
+
+  constructor(
+    private authService: AuthService,
+    private recordsService: MedicalRecordControllerService,
+    private router: Router
+  ) {
+  }
+
+  ngOnInit() {
+    const user = this.authService.getUser();
+    if (user != null) {
+      this.accountId = user.accountId;
+      this.getMyOutpatientSheets()
+    } else {
+      this.router.navigate(['/']);
+    }
+  }
+
+  private getMyOutpatientSheets() {
+    this.recordsService.getPhysicianNotes({'accountId': this.accountId}).subscribe({
+      next: (res) => {
+        this.outpatientSheets = res;
+        this.isLoading = false;
+      }
+    })
+  }
+}
