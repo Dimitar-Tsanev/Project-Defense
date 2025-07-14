6140e2b2a47d5438cdd512bbd96da748ad6a7bee
Dimitar-Tsanev <145838890+Dimitar-Tsanev@users.noreply.github.com>
2025-07-14 15:35:50 +0300

    rename component add-clinic to add-edit-clinic and now can be used for editing

--- medical-clinics-frontend/src/app/pages/add-edit-clinic/add-edit-clinic.component.ts
+++ medical-clinics-frontend/src/app/pages/add-edit-clinic/add-edit-clinic.component.ts
@@ -5,24 +5,40 @@ import {UrlDirective} from '../../utils/validators/url.directive';
 import {WorkDayDto} from '../../services/models/work-day-dto';
 import {CreateEditClinicRequest} from '../../services/models/create-edit-clinic-request';
 import {Router} from '@angular/router';
+import {ObjectTransferService} from '../../services/my-services/object-transfer-service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-add-clinic',
   imports: [
     FormsModule,
     ReactiveFormsModule,
-    UrlDirective
+    UrlDirective,
+    NgIf
   ],
-  templateUrl: './add-clinic.component.html',
-  styleUrl: './add-clinic.component.css'
+  templateUrl: './add-edit-clinic.component.html',
+  styleUrl: './add-edit-clinic.component.css'
 })
-export class AddClinicComponent implements OnInit {
+export class AddEditClinicComponent implements OnInit {
   workdays: Map<String, WorkDayDto> = new Map();
   days: Set<String> = new Set();
+  clinicId: string = '';
+  clinicEdit: boolean = false;
+
+  clinic: CreateEditClinicRequest = {
+    address: '',
+    city: '',
+    description: '',
+    identificationNumber: '',
+    phoneNumber: '',
+    pictureUrl: '',
+    workingDays: [],
+  };
 
   constructor(
     private clinicService: ClinicControllerService,
     private router: Router,
+    private objectTransfer: ObjectTransferService
   ) {
   }
 
@@ -35,10 +51,21 @@ export class AddClinicComponent implements OnInit {
       .add('FRIDAY')
       .add('SATURDAY')
       .add('SUNDAY');
+
+    if (this.objectTransfer.hasObject()) {
+      this.clinic = this.objectTransfer.getObject();
+      this.clinicId = this.objectTransfer.getObject().id
+      this.clinicEdit = true;
+
+      for (let day of this.clinic.workingDays) {
+        this.workdays.set(day.dayName.toUpperCase(), day);
+      }
+
+      this.objectTransfer.clearObject();
+    }
   }
 
-
-  addClinic(form: NgForm) {
+  submit(form: NgForm) {
     if (form.invalid) {
       return;
     }
@@ -50,15 +77,21 @@ export class AddClinicComponent implements OnInit {
     }
     clinic.workingDays = clinicWorkdays;
 
-    this.clinicService.addNewClinic$Response({body: clinic}).subscribe({
-      next: (res) => {
-        const location: string | null = res.headers.get('location');
-        if (location) {
-          const clinicId = location.split('/').pop();
-          this.router.navigate(['/clinic', clinicId]);
+    if (this.clinicEdit) {
+      this.clinicService.editClinic({'clinicId': this.clinicId.toString(), body: clinic}).subscribe({
+        next: () => this.router.navigate(['/clinic', this.clinicId])
+      })
+    } else {
+      this.clinicService.addNewClinic$Response({body: clinic}).subscribe({
+        next: (res) => {
+          const location: string | null = res.headers.get('location');
+          if (location) {
+            const clinicId = location.split('/').pop();
+            this.router.navigate(['/clinic', clinicId]);
+          }
         }
-      }
-    })
+      })
+    }
   }
 
   addWorkday(newWorkday: NgForm) {
@@ -71,10 +104,15 @@ export class AddClinicComponent implements OnInit {
       startOfWorkingDay: newWorkday.value.startOfWorkingDay + ':00',
       endOfWorkingDay: newWorkday.value.endOfWorkingDay + ':00'
     }
+
     this.workdays.set(workday.dayName, workday);
   }
 
   removeWorkday(dayName: string) {
     this.workdays.delete(dayName);
+  }
+
+  showImage(form: NgForm) {
+    this.clinic.pictureUrl = form.value.pictureUrl
   }
 }
