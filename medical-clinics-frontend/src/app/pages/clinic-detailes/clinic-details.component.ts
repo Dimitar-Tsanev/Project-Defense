e29deb5f74167d47fc3b08a38297d1245bbaf88d
Dimitar-Tsanev <145838890+Dimitar-Tsanev@users.noreply.github.com>
2025-07-14 15:48:31 +0300

    add buttons for admin to be able to delete clinic edit clinic and add staff

--- medical-clinics-frontend/src/app/pages/clinic-detailes/clinic-details.component.ts
+++ medical-clinics-frontend/src/app/pages/clinic-detailes/clinic-details.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {ClinicControllerService} from '../../services/services/clinic-controller.service';
-import {ActivatedRoute, RouterLink} from '@angular/router';
+import {ActivatedRoute, Router, RouterLink} from '@angular/router';
 import {ClinicDetails} from '../../services/models/clinic-details';
 import {LoaderComponent} from '../../common/loader/loader.component';
+import {AuthService} from '../../services/my-services/auth-service';
+import {ObjectTransferService} from '../../services/my-services/object-transfer-service';
 
 
 @Component({
@@ -16,19 +18,40 @@ import {LoaderComponent} from '../../common/loader/loader.component';
 })
 export class ClinicDetailsComponent implements OnInit {
   isLoading = true;
-  clinic: ClinicDetails | null = null;
-  clinicId: String | undefined = undefined;
+  clinic: ClinicDetails | undefined = undefined;
+  clinicId: string = '';
+  isAdmin: boolean = false;
 
-  constructor(private clinicService: ClinicControllerService, private route: ActivatedRoute) {
+  constructor(
+    private clinicService: ClinicControllerService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService,
+    private objectTransfer: ObjectTransferService
+  ) {
   }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.params['clinicId'];
-    this.clinicService.getClinicInfo({ 'clinicId':id as string}).subscribe({
+    this.clinicId = this.activatedRoute.snapshot.params['clinicId'];
+    this.isAdmin = this.authService.isAdmin()
+
+    this.clinicService.getClinicInfo({'clinicId': this.clinicId}).subscribe({
       next: (res) => {
         this.clinic = res;
         this.isLoading = false;
       }
     });
   }
+
+  deleteClinic() {
+    this.clinicService.deleteClinic({'clinicId': this.clinicId}).subscribe({
+      next: () => this.router.navigate(['clinics'])
+    })
+  }
+
+  editClinic() {
+    this.objectTransfer.setObject(this.clinic);
+    this.router.navigate(['add-edit-clinic', this.clinicId]);
+  }
 }
+
